8ae12315384f0506cba68a8976a0c2a63af6682d
alumezi <arbnorlumezi@gmail.com>
2020-11-17 18:44:31 +0100

    6.13

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,25 +1,6 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-export const vote = (id) => {
+export const vote = id => {
   return {
     type: 'VOTE',
     data: {
@@ -28,7 +9,7 @@ export const vote = (id) => {
   }
 }
 
-export const createAnecdote = (content) => {
+export const createAnecdote = content => {
   return {
     type: "CREATE",
     data: {
@@ -39,8 +20,17 @@ export const createAnecdote = (content) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+export const setAnecdotes = data => {
+  return {
+    type: "SET_ANECDOTES",
+    data
+  }
+}
+
+const reducer = (state = [], action) => {
   switch (action.type) {
+    case 'SET_ANECDOTES':
+      return action.data
     case 'VOTE':
       const id = action.data.id;
       const anecdote = state.find(item => item.id === id)
